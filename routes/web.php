6bc4695477c1f019ed28bc90ccbe724f9d874b04
Zahinul <zk.ajmain@gmail.com>
2023-11-18 07:08:31 +0600

    commited

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+Route::get('/profile/{id}', [ProfileController::class, 'index']);
